990ee090af71c072e90a6233d6d7c4e84e0c7f71
Mateus Mota <82921597+MateusMotams@users.noreply.github.com>
2025-10-07 20:10:17 -0300

    Finalização da API
    Atualização dos controladores e serviços da API

--- src/developers/developers.controller.ts
+++ src/developers/developers.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
 import { DevelopersService } from './developers.service';
 import { CreateDeveloperDto } from './dto/create-developer.dto';
 import { UpdateDeveloperDto } from './dto/update-developer.dto';
+import { not } from 'rxjs/internal/util/not';
 
 @Controller('developers')
 export class DevelopersController {
@@ -19,16 +20,20 @@ export class DevelopersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.developersService.findOne(+id);
+    return this.developersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDeveloperDto: UpdateDeveloperDto) {
-    return this.developersService.update(+id, updateDeveloperDto);
+  async update(@Param('id') id: string, @Body() updateDeveloperDto: UpdateDeveloperDto) {
+    //Quando o developer não for encontrado, lança uma exceção retornando 404
+    const developer = await this.developersService.update(id,  updateDeveloperDto);
+      if (!developer) throw new NotFoundException();
+    return developer;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.developersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const developer = await this.developersService.remove(id);
+    if (!developer) throw new NotFoundException();
   }
 }
